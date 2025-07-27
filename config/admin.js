2ef6b9767d9209b4b8965811bf1cb8e7694b6be5
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-07-28 02:21:31 +0530

    .....

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("nwDFKEjiL6ooXJbY1kaXVQ=="),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("+mecxmHVH0QcqqO/iYrdSg=="),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("ScwNX/CeDbtjw+7r4ydypA=="),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
