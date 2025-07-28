ce5f7e516118fb9be764e8ecc89389c7e361fc1d
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-07-28 23:02:57 +0530

    a

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,9 +7,14 @@ export default ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL") && {
-        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false),
-      },
+      ssl: env.bool("DATABASE_SSL")
+        ? {
+            rejectUnauthorized: env.bool(
+              "DATABASE_SSL_REJECT_UNAUTHORIZED",
+              false
+            ),
+          }
+        : false,
     },
   },
 });
