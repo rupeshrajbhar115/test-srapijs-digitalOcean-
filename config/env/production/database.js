e3a572f84df7ee86698f9a746e08f2edace83988
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-07-29 15:41:49 +0530

    ..

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,35 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "mysql2",
+//     connection: {
+//       host: env("DATABASE_HOST"),
+//       port: env.int("DATABASE_PORT"),
+//       database: env("DATABASE_NAME"),
+//       user: env("DATABASE_USERNAME"),
+//       password: env("DATABASE_PASSWORD"),
+//       ssl: {
+//         rejectUnauthorized: true, // for self-signed, use false
+//       },
+//     },
+//   },
+// });
+
+const fs = require("fs");
+const path = require("path");
+
 module.exports = ({ env }) => ({
   connection: {
     client: "mysql2",
     connection: {
       host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
+      port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
       ssl: {
-        rejectUnauthorized: true, // for self-signed, use false
+        ca: fs
+          .readFileSync(path.join(__dirname, "./ca-certificate.crt"))
+          .toString(),
       },
     },
   },
