68896e7a28a463abf64519fd0820f85360f090dd
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-07-31 15:36:09 +0530

    1111

--- src/api/home/routes/home.js
+++ src/api/home/routes/home.js
@@ -6,4 +6,4 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter("api::home.home11111");
+module.exports = createCoreRouter("api::home.home");
