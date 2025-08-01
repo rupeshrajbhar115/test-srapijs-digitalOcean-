7deb29070f37d3e73a365156d16874d02fe61a33
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-08-01 20:41:28 +0530

    Add DigitalOcean Spaces upload provider to Strapi
    Configured the Strapi upload plugin to use the 'strapi-provider-upload-do' provider for DigitalOcean Spaces. Added 'strapi-provider-upload-do' as a dependency in package.json to enable file uploads to DigitalOcean Spaces.

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env("DO_SPACE_KEY"),
+        secret: env("DO_SPACE_SECRET"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_NAME"),
+        region: env("DO_SPACE_REGION"),
+      },
+    },
+  },
+});
