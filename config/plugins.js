d08462bafcadd999f4f1e0fbb1e1511945bdb37b
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-08-01 22:27:23 +0530

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env("DO_SPACE_KEY"),
+        secret: env("DO_SPACE_SECRET"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_NAME"),
+        region: env("DO_SPACE_REGION"),
+        cdn: env("DO_SPACE_CDN"), // Optional, used for image URLs
+      },
+    },
+  },
+});
